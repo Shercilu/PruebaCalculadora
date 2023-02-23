5c7c73f19cf3dd58338fe907891e5832a6c05558
Patricia Soriano <patriciagarciasoriano@gmail.com>
2023-02-23 11:14:49 +0100

    Mi primer commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 3;
+        int resultado;
+
+        System.out.println("Calculadora de Patricia");
+        //sumar
+        System.out.println("Se realizará la operación de suma de número 1 y número 2");
+        resultado = num1 + num2;
+        System.out.println("El resultado de la suma es: " + resultado);
+
+        //restar
+        System.out.println("Se realizará la operación de resta de número 1 y número 2");
+        resultado = num1 - num2;
+        System.out.println("El resultado de la resta es: " + resultado);
+
+        //Multiplicar
+        System.out.println("Se realizará la operación de multiplicación de número 1 y número 2");
+        resultado = num1 * num2;
+        System.out.println("El resultado de la multiplicación es: " + resultado);
+
+        //Dividir
+        System.out.println("Se realizará la operación de división de número 1 y número 2");
+        resultado = num1 / num2;
+        System.out.println("El resultado de la división es: " + resultado);
+
+    }
+}
